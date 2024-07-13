46330a69a3724789b54af249a39c62a50db561fd
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-07-13 15:02:53 +0100

    url config

--- bornpikin/urls.py
+++ bornpikin/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user-user/', include('userAuth.urls')),
+    path('user-auth/', include('userAuth.urls')),
     ]
